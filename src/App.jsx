35497094feac6ad838ba7733b4dfca391bab667d
cristaincr01-stack <cristaincr01@gmail.com>
2026-08-25 01:12:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -534,6 +534,21 @@ const [editandoProducto, setEditandoProducto] = useState(false);
     localStorage.setItem("mercadoCU_sesion", sesion);
   }
 }, []);
+  useEffect(() => {
+  let sesion = localStorage.getItem("mercadoCU_sesion");
+
+  if (!sesion) {
+    sesion =
+      "SES-" +
+      Date.now().toString(36) +
+      "-" +
+      Math.random().toString(36).substring(2, 8);
+
+    localStorage.setItem("mercadoCU_sesion", sesion);
+  }
+
+  alert("Sesión MercadoCU: " + sesion);
+}, []);
   const iniciarSesion = async () => {
   if (!whatsappSesion || !pinSesion) {
     setSesionError("Escribe tu WhatsApp y contraseña");

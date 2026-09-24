d363a554172940c1d1371304e8bc1255e770dda7
cristaincr01-stack <cristaincr01@gmail.com>
2026-09-24 01:26:57 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect } from "react";
 import { MapPin, X, MessageCircle, Store, ChevronRight, Tag, Check, Lock, SlidersHorizontal, UserRound, LogIn, Settings, CircleHelp, Headphones, Info, Home, Search, Plus, Package, MoreHorizontal, Flame, BadgeCheck, Truck, Eye,
-EyeOff, LogOut, Heart, Share2, ImageIcon, ShoppingCart, Smartphone, ChevronDown, LayoutGrid, ShoppingBag, Car, Pill, ChevronLeft, UserPlus, Sparkles, Crown, Megaphone, BriefcaseBusiness, Instagram, Facebook, Music2, Youtube } from "lucide-react";
+EyeOff, LogOut, Heart, Share2, ImageIcon, ShoppingCart, Smartphone, ChevronDown, LayoutGrid, ShoppingBag, Car, Pill, ChevronLeft, UserPlus, Sparkles, Crown, Megaphone, BriefcaseBusiness, Instagram, Facebook, Music2, Youtube, Star, User } from "lucide-react";
 window.onerror = function (mensaje, archivo, linea, columna, error) {
   document.body.innerHTML = `
     <div style="
@@ -2704,7 +2704,7 @@ if (orden === "precioMayor") {
         <div className="flex items-center gap-4">
 
           <div className="w-16 h-16 rounded-2xl bg-[#202629] flex items-center justify-center shrink-0">
-            <UserRound className="w-7 h-7 text-[#69757B]" />
+            <User className="w-7 h-7 text-[#69757B]" />
           </div>
 
           <div className="flex-1">

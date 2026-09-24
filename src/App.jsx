36fad7e05b7e6605b412ad60203206fb7168ddf0
cristaincr01-stack <cristaincr01@gmail.com>
2026-09-23 21:48:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4783,7 +4783,7 @@ onChange={(e) => setPinSesion(e.target.value)}
     <div className="relative">
 
       {/* CABECERA */}
-      <div className="relative z-20 bg-[#0D1113]/85 backdrop-blur-xl border-b border-[#2A3033]">
+      <div className="relative z-20 bg-[#0D1113]/45 backdrop-blur-xl border-b border-white/5">
 
         <div className="max-w-2xl mx-auto px-4 py-3 flex items-center">
 
